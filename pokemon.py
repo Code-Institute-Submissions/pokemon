45e39ff4073c8743259892dd748b8bfa502eaf2f
FutoiSaru <futoisaru@gmail.com>
2017-06-05 15:06:59 +0100

    Added connection to MongoDB and ability to create JSON

--- pokemon.py
+++ pokemon.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
-import pprint
 import json
 
 app = Flask(__name__)
@@ -29,14 +28,17 @@ def charts():
 
 @app.route("/pokemonproject")
 def pokemonproject():
+    # Connection to MongoDB
     conn = MongoClient(MONGODB_HOST, MONGODB_PORT)
     db = conn[DBS_NAME]
+    # Creating one JSON to work with in javascript
     pokemon_full = db.pokemon_species.aggregate([
         {
+            # Adding first type to each pokemon
             "$lookup": {
                 "from": 'pokemon_types',
                 "localField": 'id',
-                "foreignField": 'id',
+                "foreignField": 'pokemon_id',
                 "as": 'types'
             }
         },
@@ -46,24 +48,178 @@ def pokemonproject():
         {
             "$redact": {
                 "$cond": [
-                    {"$eq": ["1", "$types.slot"]},
+                    {"$eq": ["$types.slot", 1]},
                     "$$KEEP",
                     "$$PRUNE"
                 ]
             }
         },
         {
+            # Adding second type to each pokemon
+            "$lookup": {
+                "from": 'pokemon_types',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'types2'
+            }
+        },
+        {
+            "$unwind": '$types2'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$types2.slot", 2]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Adding HP to each pokemon
+            "$lookup": {
+                "from": 'pokemon_stats',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'hp'
+            }
+        },
+        {
+            "$unwind": '$hp'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$hp.stat_id", 1]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Adding attack to each pokemon
+            "$lookup": {
+                "from": 'pokemon_stats',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'atk'
+            }
+        },
+        {
+            "$unwind": '$atk'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$atk.stat_id", 2]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Adding defense to each pokemon
+            "$lookup": {
+                "from": 'pokemon_stats',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'def'
+            }
+        },
+        {
+            "$unwind": '$def'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$def.stat_id", 3]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Adding special attack to each pokemon
+            "$lookup": {
+                "from": 'pokemon_stats',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'satk'
+            }
+        },
+        {
+            "$unwind": '$satk'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$satk.stat_id", 4]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Adding special defense to each pokemon
+            "$lookup": {
+                "from": 'pokemon_stats',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'sdef'
+            }
+        },
+        {
+            "$unwind": '$sdef'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$sdef.stat_id", 5]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Adding speed to each pokemon
+            "$lookup": {
+                "from": 'pokemon_stats',
+                "localField": 'id',
+                "foreignField": 'pokemon_id',
+                "as": 'speed'
+            }
+        },
+        {
+            "$unwind": '$speed'
+        },
+        {
+            "$redact": {
+                "$cond": [
+                    {"$eq": ["$speed.stat_id", 6]},
+                    "$$KEEP",
+                    "$$PRUNE"
+                ]
+            }
+        },
+        {
+            # Creates format I want to use
             "$project": {
                 "_id": 0,
                 "id": 1,
                 "identifier": 1,
                 "gender_rate": 1,
-                "type1": "$types.type_id"
+                "type1": "$types.type_id",
+                "type2": "$types2.type_id",
+                "hp": "$hp.base_stat",
+                "attack": "$atk.base_stat",
+                "defense": "$def.base_stat",
+                "special_attack": "$satk.base_stat",
+                "special_defense": "$sdef.base_stat",
+                "speed": "$speed.base_stat"
             }
         }
     ])
 
-    pprint.pprint(list(pokemon_full))
     return json.dumps(list(pokemon_full))
 
 # Run App
