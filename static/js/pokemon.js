c590198fd89a6bb2386507e41815fa8c5c75b73f
Sean Thomas <futoisaru@gmail.com>
2017-12-11 18:17:02 +0000

    Improved responsiveness of background

---
+++ static/js/pokemon.js
@@ -0,0 +1,14 @@
+// On page load set size of background image to browser height
+$(document).ready(function(){
+    $("#backgroundimg").css({
+        "height": $(window).height() + 'px',
+        "min-width": $(window).width() + 'px'});
+    $("#welcome_image").fadeIn(1000);
+});
+
+// Resize background image if window is resized
+$(window).resize(function(){
+   $("#backgroundimg").css({
+       "height": $(window).height() + 'px',
+       "min-width": $(window).width() + 'px'});
+});
